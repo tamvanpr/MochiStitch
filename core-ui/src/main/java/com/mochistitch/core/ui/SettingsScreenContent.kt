fc584f9e2e48bf46e35a3d880112d526c8683b50
tamvanpr <tamvanpr@users.noreply.github.com>
2026-09-12 06:26:21 +0000

    Add missing import for FilterChip in SettingsScreenContent

--- core-ui/src/main/java/com/mochistitch/core/ui/SettingsScreenContent.kt
+++ core-ui/src/main/java/com/mochistitch/core/ui/SettingsScreenContent.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.size
