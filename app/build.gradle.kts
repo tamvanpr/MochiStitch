359fee582e40d57810a0d04e18d7a813e01710a7
MochiStitch <mochistitch@local>
2026-09-17 10:28:50 +0700

    Tambah dependensi material-icons-extended untuk ikon Archive dan FolderOpen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -128,6 +128,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.icons.extended)
     implementation(libs.coil.compose)
 
     testImplementation(libs.junit)
