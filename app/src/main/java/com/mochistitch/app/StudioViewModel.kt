244d55eb4a053e8c6a386054c608b8e27fb808b0
MochiStitch <mochistitch@local>
2026-09-24 17:14:20 +0700

    Tambah import Bitmap yang hilang di ViewModel

--- app/src/main/java/com/mochistitch/app/StudioViewModel.kt
+++ app/src/main/java/com/mochistitch/app/StudioViewModel.kt
@@ -2,6 +2,7 @@ package com.mochistitch.app
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
