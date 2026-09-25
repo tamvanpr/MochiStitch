7a5c0af706596297ed38b1fd3c05145ba5e703ca
MochiStitch <mochistitch@local>
2026-09-25 09:45:14 +0700

    Perbaiki import background modifier

--- core-ui/src/main/java/com/mochistitch/core/ui/SlicePreview.kt
+++ core-ui/src/main/java/com/mochistitch/core/ui/SlicePreview.kt
@@ -1,6 +1,7 @@
 package com.mochistitch.core.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
